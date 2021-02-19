d64a9474166465ab59ff3722f269adb276152af1
Marco Lanzotti <marco.lanzotti@yourvoice.com>
2021-02-19 15:24:12 +0100

    Updated package informations

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="prometheus-freeswitch-exporter",
-    version="1.0.0",
-    author="Lorenz Schori",
-    author_email="lo@znerol.ch",
-    description=("FreeSWITCH exporter for the Prometheus monitoring system."),
+    name="prometheus-my-freeswitch-exporter",
+    version="0.1.0",
+    author="Marco Lanzotti"
+    author_email="marco@lanzotti.com"
+    description=("FreeSWITCH exporter for the Prometheus monitoring system based on Lorenz Schori prometheys-freeswitch-exporter."),
     long_description=codecs.open('README.rst', encoding='utf-8').read(),
     license="Apache Software License 2.0",
     keywords="prometheus exporter network monitoring voip",
-    url="https://github.com/znerol/prometheus-freeswitch-exporter",
+    url="https://github.com/rastakozza/prometheus-my-freeswitch-exporter",
     package_dir={"": "src"},
     packages=find_packages('src'),
     entry_points={
         'console_scripts': [
-            'freeswitch_exporter=freeswitch_exporter.cli:main',
+            'my_freeswitch_exporter=freeswitch_exporter.cli:main',
         ],
     },
     test_suite="tests",
